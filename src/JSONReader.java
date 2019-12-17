37118c351289d93eeb98062ed39cf5ac3f1c945f
Avery2 <JustinAveryChan@gmail.com>
2019-12-16 19:43:17 -0600

    JSON parsing init success

--- src/JSONReader.java
+++ src/JSONReader.java
@@ -1,4 +1,23 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
 public class JSONReader {
+  public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+    // parsing file "JSONExample.json"
+    JSONArray ja = (JSONArray) new JSONParser().parse(new FileReader("search-MyActivity.json"));
+    // typecasting obj to JSONObject
+    JSONObject o1 = (JSONObject) ja.get(0);
+    
+    System.out.println(o1.get("title"));
 
+//    System.out.println(ja.get(0));
+//    System.out.println(ja.get(1));
+//    System.out.println(ja.get(2));
+  }
 }
