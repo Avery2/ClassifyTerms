80f60f995fa6705e02b24625d45f2c85f418217c
Avery2 <JustinAveryChan@gmail.com>
2019-12-17 05:27:49 -0600

    entire CSV file can now be created, just ironing out some bugs

--- src/ClassifyTerms.java
+++ src/ClassifyTerms.java
@@ -2,12 +2,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import com.google.cloud.language.v1.AnalyzeSentimentResponse;
 import com.google.cloud.language.v1.ClassificationCategory;
 import com.google.cloud.language.v1.ClassifyTextRequest;
 import com.google.cloud.language.v1.ClassifyTextResponse;
 import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.Document.Type;
 import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
 
 public class ClassifyTerms {
 
@@ -15,61 +17,127 @@ public class ClassifyTerms {
   private static ClassifyTextResponse response;
 
   public static void main(String[] args) throws IOException {
-    String content = null;
-    System.out.println("start");
-
-    // TODO have json file reader instead
-//    File file = new File("myactivity.txt");
-//    BufferedReader inputStream = new BufferedReader(new FileReader(file));
-
-    // Instantiate the Language client com.google.cloud.language.v1.LanguageServiceClient
-    try (LanguageServiceClient language = LanguageServiceClient.create()) {
-      // set content to the text string
-      // content = "google calendar automatic data collection"; // works
-//      for (int i = 0; i < 5; i++) {
-//        content = inputStream.readLine();
-//        while (content.trim().length() < 1) {
-//          content = inputStream.readLine();
-//        }
-//      }
-      
-      content = "Concatenate a string like in printf - Stack Overflow";
-      
-      content = correctTokenAmount(content);
-
-      System.out.print(classifyContent(language, content));
-    } catch (com.google.api.gax.rpc.InvalidArgumentException e) {
-      System.out.println("not enough tokens, probably");
-      System.out.println();
-    }
-
-//    inputStream.close();
-    System.out.println("end");
+    // String content = null;
+    // System.out.println("start");
+    //
+    // // TODO have json file reader instead
+    // // File file = new File("myactivity.txt");
+    // // BufferedReader inputStream = new BufferedReader(new FileReader(file));
+    //
+    // // Instantiate the Language client com.google.cloud.language.v1.LanguageServiceClient
+    // try (LanguageServiceClient language = LanguageServiceClient.create()) {
+    // // set content to the text string
+    // // content = "google calendar automatic data collection"; // works
+    // // for (int i = 0; i < 5; i++) {
+    // // content = inputStream.readLine();
+    // // while (content.trim().length() < 1) {
+    // // content = inputStream.readLine();
+    // // }
+    // // }
+    //
+    // content = "Concatenate a string like in printf - Stack Overflow";
+    //
+    // content = correctTokenAmount(content);
+    //
+    //// System.out.print(sentimentAndClassifyContent(language, content));
+    // } catch (com.google.api.gax.rpc.InvalidArgumentException e) {
+    // System.out.println("not enough tokens, probably");
+    // System.out.println();
+    // }
+    //
+    // // inputStream.close();
+    // System.out.println("end");
   }
 
-  private static String classifyContent(LanguageServiceClient language, String content) {
+  public static String sentimentAndClassifyContent(LanguageServiceClient language, String content) {
 
     String output = "";
+    String conf;
+    String[] subject;
 
-    // set content (into correct format) TODO do I have to build this every time?
-    Document doc = Document.newBuilder().setContent(content).setType(Type.PLAIN_TEXT).build();
-    request = ClassifyTextRequest.newBuilder().setDocument(doc).build();
-    // detect categories in the given text
-    response = language.classifyText(request);
+    Document doc = Document.newBuilder().setContent(content).setLanguage("en")
+        .setType(Type.PLAIN_TEXT).build();
+    // set content (into correct format)
+    try {
+      request = ClassifyTextRequest.newBuilder().setDocument(doc).build();
+      // detect categories in the given text
+      response = language.classifyText(request);
 
-    if (response.getCategoriesCount() == 0) {
-      System.out.println("classification failed.");
+
+
+      // write classification
+      // name: "/Arts & Entertainment/TV & Video/TV Shows & Programs"\nconfidence: 0.83
+
+      // confidence: 0.83
+      //
+      // Arts & Entertainment
+      // TV & Video
+      // TV Shows & Programs
+      // 1
+
+      if (response.getCategoriesCount() == 0) { // if failed
+        output += ",,,";
+      } else {
+        conf = response.getCategories(0).toString().split("\n")[1].split(" ")[1];
+        subject = response.getCategories(0).toString().split("\"")[1].split("/");
+
+
+//        for (String s : subject) {
+//          System.out.println(s);
+//        }
+        if (subject.length < 3) {
+          output += subject[1] + "," + "," + conf + ",";
+        } else {
+          output += subject[1] + "," + subject[2] + "," + conf + ",";
+        }
+      }
+
+
+
+    } catch (com.google.api.gax.rpc.InvalidArgumentException e) {
+      System.out.println("not enough tokens, probably");
+    } catch (com.google.api.gax.rpc.ResourceExhaustedException e) {
+      System.out.println("\'slow down buddy\' -google");
+      try {
+        Thread.sleep(60001);
+      } catch (InterruptedException e1) {
+        e1.printStackTrace();
+      }
     }
 
-    for (ClassificationCategory category : response.getCategoriesList()) {
-      output += String.format("Category name : %s, Confidence : %.3f\n", category.getName(),
-          category.getConfidence());
+    // int i = 0;
+    // for (ClassificationCategory category : response.getCategoriesList()) {
+    //// if (i >= 3) { // TODO, what do i do? just have one category?
+    //// break;
+    //// }
+    // output += String.format("Category name : %s, Confidence : %.3f\n", category.getName(),
+    // category.getConfidence());
+    // i++;
+    // }
+    // if (i < 3) { // allow for maximum of 3 categories, and allow CSV format to allow this
+    // for (int j = i; j <= 3; j++) {
+    // output += "\n";
+    // }
+    // }
+
+    // sentiment analysis
+
+    AnalyzeSentimentResponse response = language.analyzeSentiment(doc);
+    Sentiment sentiment = response.getDocumentSentiment();
+    if (sentiment == null) {
+      System.out.println("sentiment failed");
+      output += (",,");
+    } else {
+      // output += String.format("Sentiment magnitude: %.3f,", sentiment.getMagnitude());
+      output += sentiment.getMagnitude() + ",";
+      // output += String.format("Sentiment score: %.3f,", sentiment.getScore());
+      output += sentiment.getScore() + ",";
     }
 
     return output;
   }
 
-  private static String correctTokenAmount(String input) {
+  public static String correctTokenAmount(String input) {
     // Important: You must supply a text block (document) with at least twenty tokens (words) to
     // the classifyText method.
     String output = input;
--- src/JSONReader.java
+++ src/JSONReader.java
@@ -1,23 +1,100 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
+import java.io.PrintWriter;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
+import com.google.cloud.language.v1.LanguageServiceClient;
 
 public class JSONReader {
-  public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
-    // parsing file "JSONExample.json"
-    JSONArray ja = (JSONArray) new JSONParser().parse(new FileReader("search-MyActivity.json"));
-    // typecasting obj to JSONObject
-    JSONObject o1 = (JSONObject) ja.get(0);
-    
-    System.out.println(o1.get("title"));
 
-//    System.out.println(ja.get(0));
-//    System.out.println(ja.get(1));
-//    System.out.println(ja.get(2));
+  /**
+   * Will read in data from JSON file and output wanted information as a CSV file
+   * 
+   * @param args
+   * @throws FileNotFoundException
+   * @throws IOException
+   * @throws ParseException
+   */
+  public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+    JSONArray myActivity =
+        (JSONArray) new JSONParser().parse(new FileReader("search-MyActivity.json"));
+    PrintWriter writer =
+        new PrintWriter("myactivity-" + java.time.LocalTime.now() + ".csv", "UTF-8");
+
+    writer.println(
+        "Year,Month,Day,Hour,Title,HasLocation,Classification1,Classification2,Confidence,Magnitude,Sentiment");
+
+    // typecasting obj to JSONObject
+    JSONObject temp;
+
+    int i = 0;
+    try (LanguageServiceClient language = LanguageServiceClient.create()) {
+
+      // temp = (JSONObject) myActivity.get(0);
+      // 2019-12-16T19:54:35.148Z
+      // System.out.println(temp.get("time"));
+      // writer.println(temp.get("time"));
+
+      String[] time;
+      // TODO
+      for (Object o : myActivity) {
+        System.out.println(i);
+        // setup
+        i++;
+        temp = (JSONObject) o;
+
+        time = parseTime(temp.get("time").toString()); // write CSV time
+        for (String s : time) {
+          writer.print(s + ",");
+        }
+
+        writer.print(temp.get("title").toString().replace(",", "") + ",");
+
+        if (temp.get("locationInfos") != null) {
+          writer.print("true,");
+        } else {
+          writer.print("false,");
+        }
+
+        // attempt sentiment analysis, then classify
+        writer.print(ClassifyTerms.sentimentAndClassifyContent(language,
+            ClassifyTerms.correctTokenAmount(temp.get("title").toString())) + ",");
+
+        writer.println();
+
+      }
+    } catch (Exception e) {
+      System.out.println("i: " + i);
+      e.printStackTrace();
+    }
+
+    writer.close();
+  }
+
+  /**
+   * Take in time string, return as array in [Y, M, D, H] order
+   * 
+   * @param time
+   * @return
+   */
+  private static String[] parseTime(String time) {
+    String[] temp = time.split("-");
+    String[] output = new String[4];
+    // 2019-12-16T19:54:35.148Z
+    // 2019
+    output[0] = temp[0]; // year
+    // 12
+    output[1] = temp[1]; // month
+
+    temp = temp[2].split("T");
+
+    // 16
+    output[2] = temp[0]; // day
+    // 19 or 20
+    output[3] = temp[1].split(":")[0]; // hour
+
+    return output;
   }
 }
